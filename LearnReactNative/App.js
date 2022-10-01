2da0430ea2703af8a10c0b69ca67451a809d3d0e
polarisjyb <polaris_lsj@naver.com>
2022-10-01 22:27:42 +0900

    로그인, 회원가입, 이메일 인증, 비밀번호 찾기 및 재설정 화면 구현 ( 더 디테일한 기능 및 화면 구현 필요 )

--- LearnReactNative/App.js
+++ LearnReactNative/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import {SafeAreaView, Text, StyleSheet} from 'react-native';
 import WelcomeDTG from './src/components/WelcomeDTG/WelcomeDTG';
-import SigninScreen from './src/screens/SignInScreen/SignInScreen';
+import SignInScreen from './src/screens/SignInScreen/SignInScreen';
 import SignUpScreen from './src/screens/SignUpScreen/SignUpScreen';
 import ConfirmEmailScreen from './src/screens/ConfirmEmailScreen';
 import ForgotPasswordScreen from './src/screens/ForgotPasswordScreen';
@@ -11,7 +11,7 @@ import NewPasswordScreen from './src/screens/NewPasswordScreen';
 const App = () => {
   return (
     <SafeAreaView style={styles.root}>
-      {/* <SigninScreen /> */}
+      {/* <SignInScreen /> */}
       {/* <SignUpScreen /> */}
       {/* <ConfirmEmailScreen /> */}
       {/* <ForgotPasswordScreen /> */}
